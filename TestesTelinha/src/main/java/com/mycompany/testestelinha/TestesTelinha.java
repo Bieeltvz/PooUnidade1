c2cc93a013feebcf654071df5e981e017c4f3abe
Gabriel Tormena <110978689+Bieeltvz@users.noreply.github.com>
2025-03-19 19:18:00 -0300

    Add files via upload

---
+++ TestesTelinha/src/main/java/com/mycompany/testestelinha/TestesTelinha.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testestelinha;
+
+/**
+ *
+ * @author gtvargas
+ */
+public class TestesTelinha {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
