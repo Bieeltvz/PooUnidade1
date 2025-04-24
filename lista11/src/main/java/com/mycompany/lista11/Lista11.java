f08a5337ef4435c1cc101370f2c32d091b5e3346
Gabriel Tormena <110978689+Bieeltvz@users.noreply.github.com>
2025-04-23 21:11:53 -0300

    Add files via upload

---
+++ lista11/src/main/java/com/mycompany/lista11/Lista11.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista11;
+
+/**
+ *
+ * @author gtvargas
+ */
+public class Lista11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
