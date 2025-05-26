f3aca4845c64c27e1d215c6b9e10ea038ebe03d2
Gabriel Tormena <110978689+Bieeltvz@users.noreply.github.com>
2025-05-26 19:59:31 -0300

    Add files via upload

---
+++ ExemploIgualdade/src/main/java/com/mycompany/exemploigualdade/Pessoa.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemploigualdade;
+
+/**
+ *
+ * @author gtvargas
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        setNome(nome);
+        setIdade(idade);
+    }
+
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if (obj == null){
+            return false;
+        }
+        if (this.getClass() != obj.getClass()){
+            return false;
+        }
+        Pessoa p = (Pessoa) obj;
+        
+        if (this.nome.equals(p.getNome())){
+            return false;
+        }
+        
+        if (this.idade != p.getIdade()){
+            return false;
+        }
+        return true;
+    }
+}
